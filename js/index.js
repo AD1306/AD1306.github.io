c99a3518fbe7ccb54c6fb0c317d3d3d81aa65c94
Aditya Dave <Aditya.Dave@zaytion.com>
2019-04-22 16:55:02 -0400

    new portfolio

--- js/index.js
+++ js/index.js
@@ -9,15 +9,14 @@ $(document).ready(function () {
     navigation: true,
     slidesNavigation: true,
     navigationPosition: 'right',
-    navigationTooltips: ['Home', 'About', 'Skills', 'Contact'],
+    navigationTooltips: ['HOME', 'ABOUT', 'SKILLS', 'CONTACT'],
     afterLoad: function (anchorLink, index) {
       var loadedSection = $(this);
       if (anchorLink === 'skills') {
         $("#homeNav").hide();
-        skillsContent();
       } else if (anchorLink === 'about') {
         $("#homeNav").hide();
-        $("#aboutDisplay").fadeIn("slow");
+        startLogoAnimation();
       } else if (anchorLink === 'home') {
         $("#aboutNav").show();
         $("#homeNav").hide();
@@ -27,38 +26,56 @@ $(document).ready(function () {
       }
     }
   });
-
   $(".section").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function (e) { });
-  $("#frontend").hide();
-  $("#backend").hide();
-  $("#cloud").hide();
-  $("#other").hide();
   $("#homeNav").hide();
-  $("#aboutDisplay").hide();
-  $("#contactDisplay").hide();
+  $('.skillsTree').show();
   var typed = new Typed('#speciality', {
     strings: ["I'm a Front-End Developer", "I'm a Back-End Developer", "I'm a Full-Stack Developer"],
     smartBackspace: true,
     typeSpeed: 30,
     backSpeed: 50,
-    cursorChar: '|',
+    cursorChar: '_',
     loop: true,
     loopCount: Infinity,
     startDelay: 500
   });
+  generateCloud();
   $(".typed-cursor").css("font-size", "xx-large");
   $(window).on('resize', resizeFunction)
   checkCurrentWindow();
-  $(".button-collapse").sideNav();
   $(".side-nav li").on('click', hideMenu);
   $(window).on('hashchange', hideMenu);
+  $('.switch label input').click(switchSkills);
 });
 
+function switchSkills() {
+  if ($('.switch label input').is(":checked")) {
+    $('.skillsTree').hide();
+    $('.skillsCloud').show();
+  } else {
+    $('.skillsCloud').hide();
+    $('.skillsTree').show();
+  }
+}
+
 function checkCurrentWindow() {
-  if($("#mobileMenu").is(":visible") && $(window).width() < 993) {
+  if ($("#mobileMenu").is(":visible") && $(window).width() < 993) {
     $("#navbar").hide()
+    $(".button-collapse").sideNav();
+    $("#fp-nav").hide();
+    $("#footer").css({ "bottom": "10px", "height": "10vh" });
+    $("#footer-contact").css({ "bottom": "10px", "height": "10vh" });
+    $('.skillsTree').hide();
+    $('.skillsCloud').show();
+    $('.optionsContainer').hide();
   } else {
     $("#navbar").show()
+    $("#fp-nav").show();
+    $("#footer").css({ "bottom": "10px", "height": "10vh" });
+    $("#footer-contact").css({ "bottom": "10px", "height": "10vh" });
+    $('.skillsCloud').hide();
+    $('.skillsTree').show();
+    $('.optionsContainer').show();
   }
 }
 
@@ -67,28 +84,114 @@ function resizeFunction() {
   if (!eventFired) {
     if ($("#mobileMenu").is(":visible") && $(window).width() < 993) {
       $("#navbar").hide()
+      $(".button-collapse").sideNav();
+      $("#fp-nav").hide();
+      $("#footer").css({ "bottom": "10px", "height": "10vh" });
+      $("#footer-contact").css({ "bottom": "10px", "height": "10vh" });
+      $('.skillsTree').hide();
+      $('.skillsCloud').show();
+      $('.optionsContainer').hide();
     } else {
       $("#navbar").show()
+      $("#fp-nav").show();
+      $("#footer").css({ "bottom": "10px", "height": "10vh" });
+      $("#footer-contact").css({ "bottom": "10px", "height": "10vh" });
+      $('.skillsCloud').hide();
+      $('.skillsTree').show();
+      $('.optionsContainer').show();
     }
+
   }
-}
-
-
-function skillsContent() {
-  $("#frontend").fadeIn("slow");
-  $("#backend").fadeIn("slow");
-  $("#cloud").fadeIn("slow");
-  $("#other").fadeIn("slow");
 }
 
 function hideMenu() {
   if ($('#sidenav-overlay').length > 0 && sidenav === true) {
-    $("#menu-trigger").click(); 
+    $("#menu-trigger").click();
     sidenav = false;
   } else {
     sidenav = true;
-  } 
+  }
 }
 
+function generateCloud() {
+  var entries = [
+    { label: 'HTML 5', url: '#', target: '_top' },
+    { label: 'CSS3', url: '#', target: '_top' },
+    { label: 'ANGULAR 2+', url: '#', target: '_top' },
+    { label: 'DJANGO', url: '#', target: '_top' },
+    { label: 'JAVASCRIPT', url: '#', target: '_top' },
+    { label: 'TYPESCRIPT', url: '#', target: '_top' },
+    { label: 'MATERIALIZE CSS', url: '#', target: '_top' },
+    { label: 'BOOTSTRAP', url: '#', target: '_top' },
+    { label: 'JQUERY', url: '#', target: '_top' },
+    { label: 'JAVA', url: '#', target: '_top' },
+    { label: 'C#', url: '#', target: '_top' },
+    { label: 'PYTHON', url: '#', target: '_top' },
+    { label: 'DOTNET CORE', url: '#', target: '_top' },
+    { label: 'NODE JS', url: '#', target: '_top' },
+    { label: 'EXPRESS JS', url: '#', target: '_top' },
+    { label: 'GRAPHQL', url: '#', target: '_top' },
+    { label: 'IBM BLUEMIX', url: '#', target: '_top' },
+    { label: 'AZURE DEVOPS', url: '#', target: '_top' },
+    { label: 'FIREBASE', url: '#', target: '_top' },
+    { label: 'DOCKER', url: '#', target: '_top' },
+    { label: 'NGINX', url: '#', target: '_top' },
+    { label: 'MONGODB', url: '#', target: '_top' },
+    { label: 'AZURE COSMOS DB', url: '#', target: '_top' },
+    { label: 'COUCH DB', url: '#', target: '_top' },
+    { label: 'MSSQL', url: '#', target: '_top' },
+    { label: 'MYSQL', url: '#', target: '_top' },
+    { label: 'POSTGRESQL', url: '#', target: '_top' },
+    { label: 'GIT', url: '#', target: '_top' },
+    { label: 'BITBUCKET', url: '#', target: '_top' },
+    { label: 'GITHUB', url: '#', target: '_top' },
+    { label: 'BASH SCRIPTS', url: '#', target: '_top' },
+    { label: 'PYTHON SCRIPTS', url: '#', target: '_top' },
+    { label: 'LETSENCRYPT', url: '#', target: '_top' }
+  ];
+  var settings = {
 
+    entries: entries,
+    width: 800,
+    height: 800,
+    radius: '70%',
+    radiusMin: 75,
+    bgDraw: true,
+    bgColor: 'none',
+    opacityOver: 1.00,
+    opacityOut: 0.05,
+    opacitySpeed: 6,
+    fov: 800,
+    speed: 1,
+    fontFamily: 'Oswald, Arial, sans-serif',
+    fontSize: '15',
+    fontColor: '#4db6ac',
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    fontStretch: 'normal',
+    fontToUpperCase: true,
+  };
+  $('#tag-cloud').svg3DTagCloud(settings);
 
+}
+
+function startLogoAnimation() {
+  anime({
+    targets: '.line-drawing-demo .lines ellipse',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutCirc',
+    duration: 2000,
+    delay: function (el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: false
+  });
+  anime({
+    targets: '.line-drawing-demo .lines path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutQuint',
+    duration: 2000,
+    delay: function (el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+  });
+}
---
+++ js/skilltree.js
@@ -0,0 +1,415 @@
+var treeData = {
+    "name": "SKILLS",
+    "children": [
+        {
+            "name": "FRONT-END",
+            "children": [
+                {
+                    "name": "CORE",
+                    "children": [
+                        {
+                            "name": "HTML5",
+                            "children": []
+                        },
+                        {
+                            "name": "CSS3",
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "name": "FRAMEWORKS",
+                    "children": [
+                        {
+                            "name": "ANGULAR",
+                            "children": []
+                        },
+                        {
+                            "name": "DJANGO",
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "name": "LANGUAGES",
+                    "children": [
+                        {
+                            "name": "JAVASCRIPT",
+                            "children": []
+                        },
+                        {
+                            "name": "TYPESCRIPT",
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "name": "UI FRAMEWORKS",
+                    "children": [
+                        {
+                            "name": "MATERIALIZECSS",
+                            "children": []
+                        },
+                        {
+                            "name": "BOOTSTRAP",
+                            "children": []
+                        },
+                        {
+                            "name": "JQUERY",
+                            "children": []
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "BACK-END",
+            "children": [
+                {
+                    "name": "LANGUAGES",
+                    "children": [
+                        {
+                            "name": "JAVA",
+                            "children": []
+                        },
+                        {
+                            "name": "C#",
+                            "children": []
+                        },
+                        {
+                            "name": "PYTHON",
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "name": "API",
+                    "children": [
+                        {
+                            "name": "RESTFUL",
+                            "children": [
+                                {
+                                    "name": "DOTNET CORE",
+                                    "children": []
+                                },
+                                {
+                                    "name": "NODEJS (EXPRESSJS)",
+                                    "children": []
+                                }
+                            ]
+                        },
+                        {
+                            "name": "GRAPHQL",
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "name": "CLOUD",
+                    "children": [
+                        {
+                            "name": "IBM BLUEMIX",
+                            "children": []
+                        },
+                        {
+                            "name": "AZURE DEVOPS",
+                            "children": []
+                        },
+                        {
+                            "name": "FIREBASE",
+                            "children": []
+                        },
+                        {
+                            "name": "DOCKER",
+                            "children": []
+                        },
+                        {
+                            "name": "NGINX",
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "name": "DATABASE",
+                    "children": [
+                        {
+                            "name": "NOSQL",
+                            "children": [
+                                {
+                                    "name": "MONGODB",
+                                    "children": []
+                                },
+                                {
+                                    "name": "AZURE COSMOS DB",
+                                    "children": []
+                                },
+                                {
+                                    "name": "COUCHDB",
+                                    "children": []
+                                }
+                            ]
+                        },
+                        {
+                            "name": "SQL",
+                            "children": [
+                                {
+                                    "name": "MSSQL",
+                                    "children": []
+                                },
+                                {
+                                    "name": "MYSQL",
+                                    "children": []
+                                },
+                                {
+                                    "name": "POSTGRESQL",
+                                    "children": []
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "OTHER",
+            "children": [
+                {
+                    "name": "VERSION CONTROL",
+                    "children": [
+                        {
+                            "name": "GIT",
+                            "children": []
+                        },
+                        {
+                            "name": "TOOLS",
+                            "children": [
+                                {
+                                    "name": "BITBUCKET",
+                                    "children": []
+                                },
+                                {
+                                    "name": "GITHUB",
+                                    "children": []
+                                },
+                                {
+                                    "name": "AZURE",
+                                    "children": []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "name": "SCRIPTING",
+                    "children": [
+                        {
+                            "name": "BASH",
+                            "children": []
+                        },
+                        {
+                            "name": "PYTHON",
+                            "children": []
+                        }
+                    ]
+                },
+                {
+                    "name": "AUTHENTICATION",
+                    "children": [
+                        {
+                            "name": "SSL CERTIFICATES",
+                            "children": []
+                        },
+                        {
+                            "name": "OIDC/OAUTH2",
+                            "children": []
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+// Set the dimensions and margins of the diagram
+var margin = {top: 30, right: 90, bottom: 30, left: 90},
+    width = $(document).width(),
+    height = $(window).height() - margin.bottom - margin.top;
+
+// append the svg object to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("#skillsContent .skillsTree").append("svg")
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate("
+          + margin.left + "," + margin.top + ")");
+
+var i = 0,
+    duration = 750,
+    root;
+
+// declares a tree layout and assigns the size
+var treemap = d3.tree().size([height, width]);
+
+// Assigns parent, children, height, depth
+root = d3.hierarchy(treeData, function(d) { return d.children; });
+root.x0 = height / 2;
+root.y0 = 0;
+
+// Collapse after the second level
+root.children.forEach(collapse);
+
+update(root);
+
+// Collapse the node and all it's children
+function collapse(d) {
+  if(d.children) {
+    d._children = d.children
+    d._children.forEach(collapse)
+    d.children = null
+  }
+}
+
+function update(source) {
+
+  // Assigns the x and y position for the nodes
+  var treeData = treemap(root);
+
+  // Compute the new tree layout.
+  var nodes = treeData.descendants(),
+      links = treeData.descendants().slice(1);
+  // Normalize for fixed-depth.
+  nodes.forEach(function(d){ d.y = d.depth * 180});
+
+  // ****************** Nodes section ***************************
+
+  // Update the nodes...
+  var node = svg.selectAll('g.node')
+      .data(nodes, function(d) {return d.id || (d.id = ++i); });
+
+  // Enter any new modes at the parent's previous position.
+  var nodeEnter = node.enter().append('g')
+      .attr('class', 'node')
+      .attr("transform", function(d) {
+        return "translate(" + source.y0 + "," + source.x0 + ")";
+    })
+    .on('click', click);
+
+  // Add Circle for the nodes
+  nodeEnter.append('circle')
+      .attr('class', 'node')
+      .attr('r', 1e-6)
+      .style("fill", function(d) {
+          return d._children ? "#83CCC5 " : "#3C948B  ";
+      });
+
+  // Add labels for the nodes
+
+
+  nodeEnter.append('text')
+      .attr("dy", ".35em")
+      .attr("x", function(d) {
+          return d.children || d._children ? -13 : 13;
+      })
+      .style('fill', '#4db6ac')
+      .attr("text-anchor", function(d) {
+          return d.children || d._children ? "end" : "start";
+      })
+      .text(function(d) { return d.data.name; });
+
+  // UPDATE
+  var nodeUpdate = nodeEnter.merge(node);
+
+  // Transition to the proper position for the node
+  nodeUpdate.transition()
+    .duration(duration)
+    .attr("transform", function(d) { 
+        return "translate(" + d.y + "," + d.x + ")";
+     });
+
+  // Update the node attributes and style
+  nodeUpdate.select('circle.node')
+    .attr('r', 10)
+    .style("fill", function(d) {
+        return d._children ? "#83CCC5 " : "#3C948B  ";
+    })
+    .attr('cursor', 'pointer');
+
+
+  // Remove any exiting nodes
+  var nodeExit = node.exit().transition()
+      .duration(duration)
+      .attr("transform", function(d) {
+          return "translate(" + source.y + "," + source.x + ")";
+      })
+      .remove();
+
+  // On exit reduce the node circles size to 0
+  nodeExit.select('circle')
+    .attr('r', 1e-6);
+
+  // On exit reduce the opacity of text labels
+  nodeExit.select('text')
+    .style('fill-opacity', 1e-6);
+
+  // ****************** links section ***************************
+
+  // Update the links...
+  var link = svg.selectAll('path.link')
+      .data(links, function(d) { return d.id; });
+      
+  // Enter any new links at the parent's previous position.
+  var linkEnter = link.enter().insert('path', "g")
+      .attr("class", "link")
+      .attr('d', function(d){
+        var o = {x: source.x0, y: source.y0}
+        return diagonal(o, o)
+      });
+
+  // UPDATE
+  var linkUpdate = linkEnter.merge(link);
+
+  // Transition back to the parent element position
+  linkUpdate.transition()
+      .duration(duration)
+      .attr('d', function(d){ return diagonal(d, d.parent) });
+
+  // Remove any exiting links
+  var linkExit = link.exit().transition()
+      .duration(duration)
+      .attr('d', function(d) {
+        var o = {x: source.x, y: source.y}
+        return diagonal(o, o)
+      })
+      .remove();
+
+  // Store the old positions for transition.
+  nodes.forEach(function(d){
+    d.x0 = d.x;
+    d.y0 = d.y;
+  });
+
+  // Creates a curved (diagonal) path from parent to the child nodes
+  function diagonal(s, d) {
+
+    path = `M ${s.y} ${s.x}
+            C ${(s.y + d.y) / 2} ${s.x},
+              ${(s.y + d.y) / 2} ${d.x},
+              ${d.y} ${d.x}`
+
+    return path
+  }
+
+  // Toggle children on click.
+  function click(d) {
+    if (d.children) {
+        d._children = d.children;
+        d.children = null;
+      } else {
+        d.children = d._children;
+        d._children = null;
+      }
+    update(d);
+  }
+}
